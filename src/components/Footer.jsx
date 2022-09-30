3adee6e0a95620944bb0150877ffbc40f320deb1
Mehdi Barim <mbarim@weborama.com>
2022-09-30 15:53:11 +0200

    findX

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Footer = () => (
+  <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
+    &copy;  {new Date().getFullYear()} Mehdi. All Rights Reserved.
+  </div>
+);
---
+++ src/contexts/StateContextProvider.js
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const StateContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getResults = async (url) => {
+    setLoading(true);
+
+    const res = await fetch(`${baseUrl}${url}`, {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+        'x-rapidapi-key': '8444295572msh24f383cf9a09919p1c81f7jsn2d7922cb974f',
+      },
+    });
+
+    const data = await res.json();
+
+    setResults(data);
+    setLoading(false);
+  };
+
+  return (
+    <StateContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
