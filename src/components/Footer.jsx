4a6a2de9388d0d3cadae7211a23806221be8c3e5
Mehdi Barim <mbarim@weborama.com>
2022-10-11 16:16:40 +0200

    fixes 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,8 @@ import { AiFillGithub } from 'react-icons/ai';
 export const Footer = () => (
   <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
     &copy;  {new Date().getFullYear()} Mehdi. All Rights Reserved.
-    <button label type="button" className="dark:bg-gray-700 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg"><AiFillGithub /></button>
+    <button type="button" className="dark:bg-gray-500 dark:text-gray-900 bg-white border rounded-full px-1 py-1 hover:shadow-lg ml-2" href="https://github.com/DroidBarber/FindX" target="_blank">
+      <AiFillGithub />
+    </button>
   </div>
 );
