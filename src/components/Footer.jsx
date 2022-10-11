14e7ff679a7631b78ecb977d9fbb6fa089f2f692
Mehdi Barim <mbarim@weborama.com>
2022-10-11 16:20:24 +0200

    fixes 3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillGithub } from 'react-icons/ai';
 export const Footer = () => (
   <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
     &copy;  {new Date().getFullYear()} Mehdi. All Rights Reserved.
-    <button type="button" className="dark:bg-gray-500 dark:text-gray-900 bg-white border rounded-full px-1 py-1 hover:shadow-lg ml-2" href="https://github.com/DroidBarber/FindX" target="_blank">
+    <button type="button" className="dark:bg-gray-500 dark:text-gray-900 bg-white rounded-full px-1 py-1 hover:shadow-lg ml-2" href="https://github.com/DroidBarber/FindX" target="_blank">
       <AiFillGithub />
     </button>
   </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,11 @@ export const Navbar = ({ setDarkTheme, darkTheme }) => (
   <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
     <div className="flex justify-between items-center space-x-5 w-screen ">
       <Link to="/">
-        <p className="text-2xl bg-white font-bold text-blue-600 py-1 px-2 rounded dark:bg-gray-500 dark:text-white">
+        <p className="text-2xl bg-white font-bold text-blue-600 py-1 px-2 rounded dark:bg-blue-700 dark:text-white">
           FindX
         </p>
       </Link>
-      <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-700 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">{darkTheme ? '💡' : '🌙'}</button>
+      <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-700 dark:text-gray-900 bg-white  rounded-full px-2 py-1 hover:shadow-lg">{darkTheme ? '💡' : '🌙'}</button>
     </div>
     <Search />
   </div>
